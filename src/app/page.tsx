000ffea01fac2c94c04b1e9d2252cce6a266b5f7
Ahmet Burak Arslan <arslanahmet4772@gmail.com>
2024-08-24 14:18:43 +0200

    add function added and some ui fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+"use client"
+import { useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Plus, Trash, Pencil } from "lucide-react";
+import { Pencil, Plus, Trash } from "lucide-react";
 
 interface note {
   id: number
@@ -9,31 +11,43 @@ interface note {
 }
 
 export default function Home() {
+  const [data, setData] = useState<note[]>(localStorage.getItem("notes") ? JSON.parse(localStorage.getItem("notes") as string) : [])
+  const [task, setTask] = useState("")
+  const input = useRef<HTMLInputElement>(null)
 
-  const exampleData: note[] = [
-    {
-      id: 1,
-      text: "Test",
-      createdAt: new Date()
-    },
-    {
-      id: 2,
-      text: "Test 2",
+  const remove = () => {
+
+  }
+
+  const add = () => {
+    if (!task) return
+    const newNote: note = {
+      id: data.length + 1,
+      text: task,
       createdAt: new Date()
     }
-  ]
+    const storedNotes = localStorage.getItem("notes")
+    const existingNotes: note[] = storedNotes ? JSON.parse(storedNotes) : [];
+    const newNotes = [...existingNotes, newNote];
+    localStorage.setItem("notes", JSON.stringify(newNotes));
+
+    setData([...data, newNote])
+    setTask("")
+    if (input.current) input.current.value = ""
+  }
 
   return (
     <main className="flex justify-start items-center w-screen h-screen flex-col bg-zinc-900 p-10">
       <div className="w-[900px] flex flex-row gap-2 m-4">
-        <Input placeholder="Add note" />
-        <Button className="bg-zinc-700 hover:bg-zinc-600 flex items-center justify-center"><Plus /></Button>
+        <Input ref={input} placeholder="Add note" onChange={(e) => setTask(e.target.value)} />
+        <Button onClick={add} className="bg-zinc-700 hover:bg-zinc-600 flex items-center justify-center"><Plus /></Button>
       </div>
       <div className="w-full flex flex-col items-center justify-center gap-2">
-        {exampleData.length > 0 ? exampleData.map((note) => (
+        {data.length > 0 ? data.map((note) => (
           <div key={note.id} className="bg-neutral-400 w-[900px] h-20 p-4 rounded-full flex flex-row justify-between">
             <p className="text-2xl ml-3 flex items-center justify-center">{note.text}</p>
             <div className="mr-4 flex flex-row gap-2">
+              <Button className="flex items-center justify-center rounded-full w-14 h-14"><Pencil /></Button>
               <Button className="flex items-center justify-center rounded-full w-14 h-14"><Trash /></Button>
             </div>
           </div>
